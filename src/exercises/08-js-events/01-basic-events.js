a85e46c88b5c38f37f3973ba27674b236a4c34b6
Oran Phillips <N00255034@iadt.ie>
2026-03-23 12:56:11 +0000

    js ecercises 9

---
+++ src/exercises/08-js-events/01-basic-events.js
@@ -0,0 +1,31 @@
+let box = document.getElementById('box');
+let toggleBoxBtn = document.getElementById('toggle_box_btn');
+let preview = document.getElementById('preview');
+let previewInput = document.getElementById('preview_input');
+
+toggleBoxBtn.addEventListener('click', () => {
+    toggleBoxVisibility(box);
+});
+
+function toggleBoxVisibility(box2){
+    // console.log(event.target);
+    box2.classList.toggle('hidden');
+}
+
+previewInput.addEventListener('change', (event) => {
+    updatePreview(preview, event.target.value)
+})
+
+function updatePreview(previewElement, text){
+    // console.log(PreviewElement, text);
+    const trimmed = text.trim();
+
+    if(trimmed == ''){
+        previewElement.textContent = '(nothing yet)';
+        previewElement.classList.add('empty');
+    }
+    else{
+        previewElement.textContent = trimmed;
+        previewElement.classList.remove('empty');
+    }
+}
